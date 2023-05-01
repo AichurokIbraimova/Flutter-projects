798da8db339077f9b8e7fb99c50248290596b91c
Aichurok <aichurok.ibraimova.bj@gmail.com>
2023-05-01 15:36:36 +0300

    buttu

--- sabak16_bmi_and_snippets/lib/app.dart
+++ sabak16_bmi_and_snippets/lib/app.dart
@@ -7,10 +7,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData.dark(),
       home: const MyHomePage(),
     );
   }
--- sabak16_bmi_and_snippets/lib/home_page.dart
+++ sabak16_bmi_and_snippets/lib/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:sabak16_bmi_and_snippets/components/status_card.dart';
+import 'package:sabak16_bmi_and_snippets/theme/app_colors.dart';
+import 'package:sabak16_bmi_and_snippets/theme/app_texts.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -11,8 +14,62 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.bgcColor,
       appBar: AppBar(
-        title: const Text('MyHomePage'),
+        backgroundColor: AppColors.bgcColor,
+        title: const Center(child: AppTexts.bmi),
+        elevation: 0,
+      ),
+      body: Padding(
+        padding:
+            const EdgeInsets.only(left: 21, top: 32, right: 21, bottom: 41),
+        child: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  StatusCard(),
+                  SizedBox(
+                    width: 39,
+                  ),
+                  StatusCard(),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 18,
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  StatusCard(),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 18,
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  StatusCard(),
+                  SizedBox(
+                    width: 39,
+                  ),
+                  StatusCard(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        color: AppColors.pinkColor,
+        height: 73,
+        width: double.infinity,
+        child: Center(
+          child: AppTexts.calculator,
+        ),
       ),
     );
   }
