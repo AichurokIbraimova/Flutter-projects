817d9c01b51d630dff0e3299fa1babd7b99d3810
Aichurok <aichurok.ibraimova.bj@gmail.com>
2023-07-03 14:34:06 +0300

    do

--- sabak28_news_app_04/lib/view/detail_view.dart
+++ sabak28_news_app_04/lib/view/detail_view.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:sabak28_news_app_04/components/detail_description.dart';
+import 'package:sabak28_news_app_04/components/detail_divider.dart';
+import 'package:sabak28_news_app_04/components/detail_news_time.dart';
+import 'package:sabak28_news_app_04/components/detail_site_button.dart';
+import 'package:sabak28_news_app_04/components/detail_title.dart';
 import 'package:sabak28_news_app_04/model/article.dart';
-import 'package:sabak28_news_app_04/theme/app_texts_styles.dart';
+import 'package:share_plus/share_plus.dart';
 
 class DetailView extends StatelessWidget {
   const DetailView({super.key, required this.article});
@@ -15,125 +20,45 @@ class DetailView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color(0xff153059),
+        centerTitle: true,
         title: Text(
           article.title,
         ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.share),
-          )
-        ],
+        actions: article.url.isNotEmpty
+            ? [
+                IconButton(
+                  onPressed: () {
+                    Share.share(article.url);
+                  },
+                  icon: const Icon(Icons.share),
+                )
+              ]
+            : null,
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            DetailTitle(article: article),
-            const SizedBox(
-              height: 10,
-            ),
-            const DetailDivider(),
-            DetailNewsTime(newsTime: newsTime),
-            const SizedBox(
-              height: 15,
-            ),
-            Image.network(article.urlToImage.toString()),
-            const SizedBox(
-              height: 15,
-            ),
-            DetailDescription(article: article),
-            const DetailSiteButton()
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              DetailTitle(article: article),
+              const SizedBox(
+                height: 10,
+              ),
+              const DetailDivider(),
+              DetailNewsTime(newsTime: newsTime),
+              const SizedBox(
+                height: 15,
+              ),
+              Image.network(article.urlToImage.toString()),
+              const SizedBox(
+                height: 15,
+              ),
+              DetailDescription(article: article),
+              const DetailSiteButton()
+            ],
+          ),
         ),
       ),
-    );
-  }
-}
-
-class DetailTitle extends StatelessWidget {
-  const DetailTitle({
-    super.key,
-    required this.article,
-  });
-
-  final Article article;
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      article.title,
-      textAlign: TextAlign.center,
-      style: AppTextStyles.titleTextStyle,
-    );
-  }
-}
-
-class DetailSiteButton extends StatelessWidget {
-  const DetailSiteButton({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialButton(
-      color: Colors.deepOrange,
-      onPressed: () {},
-      child: Text('Сайтын коруу'),
-    );
-  }
-}
-
-class DetailDescription extends StatelessWidget {
-  const DetailDescription({
-    super.key,
-    required this.article,
-  });
-
-  final Article article;
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      article.description,
-      textAlign: TextAlign.center,
-      style: AppTextStyles.descriptionTextStyle,
-    );
-  }
-}
-
-class DetailNewsTime extends StatelessWidget {
-  const DetailNewsTime({
-    super.key,
-    required this.newsTime,
-  });
-
-  final String newsTime;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Icon(Icons.access_time),
-        const SizedBox(
-          width: 10,
-        ),
-        Text(newsTime),
-      ],
-    );
-  }
-}
-
-class DetailDivider extends StatelessWidget {
-  const DetailDivider({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return const Divider(
-      color: Colors.deepOrange,
     );
   }
 }
