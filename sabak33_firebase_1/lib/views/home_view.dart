326f1db4ac9f70afa990367848b14edeca8e1d3b
Aichurok <aichurok.ibraimova.bj@gmail.com>
2023-07-18 23:59:17 +0300

    do

---
+++ sabak33_firebase_1/lib/views/home_view.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:sabak33_firebase_1/views/todo_view.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeView'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.purple),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const TodoView()));
+                },
+                child: const Icon(
+                  Icons.arrow_forward,
+                  color: Colors.white,
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
