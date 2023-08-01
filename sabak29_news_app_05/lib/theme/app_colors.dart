1aa3d78dfdbb93977f9686a8e7d692995ee9e1c2
Aichurok <aichurok.ibraimova.bj@gmail.com>
2023-08-01 17:23:32 +0300

    done

--- sabak29_news_app_05/lib/theme/app_colors.dart
+++ sabak29_news_app_05/lib/theme/app_colors.dart
@@ -4,5 +4,5 @@ class AppColors {
   static const Color appBarColors = Color(0xffFE5722);
   static const Color cardColors = Color.fromARGB(255, 244, 240, 240);
   static const Color detailAppBArColors = Colors.orange;
-  static const Color scaffoldrColors = Colors.grey;
+  static const Color scaffoldrColors = Colors.white;
 }
